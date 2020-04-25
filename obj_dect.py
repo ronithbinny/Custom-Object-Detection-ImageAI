57b8c62d14abfaf2e45209d545d4af383392a435
Ronith <52559181+ronithbinny@users.noreply.github.com>
2020-04-25 16:29:53 +0530

    Update obj_dect.py

--- obj_dect.py
+++ obj_dect.py
@@ -18,3 +18,14 @@ trainer.setModelTypeAsYOLOv3()
 trainer.setDataDirectory(data_directory="BigData/")
 metrics = trainer.evaluateModel(model_path="BigData/models", json_path="BigData/json/detection_config.json", iou_threshold=0.5, object_threshold=0.3, nms_threshold=0.5)
 print(metrics)
+
+
+# Detection
+detector = CustomObjectDetection()
+detector.setModelTypeAsYOLOv3()
+detector.setModelPath("ens-ex-60--loss-2.76.h5") 
+detector.setJsonPath("detection_config.json")
+detector.loadModel()
+detections = detector.detectObjectsFromImage(input_image="holo3.jpg", output_image_path="holo3-detected.jpg")
+for detection in detections:
+    print(detection["name"], " : ", detection["percentage_probability"], " : ", detection["box_points"])
